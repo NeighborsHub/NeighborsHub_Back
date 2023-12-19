76317fe8f6f22d684448e9e7129675f30f54b378
milad <8590410@gmail.com>
2023-12-19 11:09:21 +0330

    install libgdal-dev

--- NeighborsHub/settings.py
+++ NeighborsHub/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'HOST': '127.0.0.1',
         'USER': 'root',
-        'PASSWORD': '123456',
+        'PASSWORD': 't00r',
         'PORT': '5432',
         'NAME': 'neighbors_hub_db',
     }
