9d606df7ade02f4495f9332fbbcfb5ce7f30c6a9
milad tavakoli <8590410@gmail.com>
2023-12-27 17:55:59 +0330

    Create post - Change url

--- post/urls.py
+++ post/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from post.views import CreateUserPostAPI
 
 urlpatterns = [
-    path('create', CreateUserPostAPI.as_view(), name='user_post_create')
+    path('me/create', CreateUserPostAPI.as_view(), name='user_post_create')
 ]
