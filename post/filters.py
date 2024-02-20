cc834b18132e633470eb2685234813ff3bd5426a
milad tavakoli <8590410@gmail.com>
2024-02-20 19:58:46 +0330

    filter post from days before

--- post/filters.py
+++ post/filters.py
@@ -1,6 +1,9 @@
+import datetime
+
 import django_filters
 from django.db.models import Q
 from rest_framework_gis.filters import InBBoxFilter
+from django.utils import timezone
 
 from .models import Post
 
@@ -8,12 +11,17 @@ from .models import Post
 class ListPostFilter(django_filters.FilterSet):
     # Define the filter for the OR operation
     hashtag_title = django_filters.CharFilter(method='hashtag_comment_or_post')
+    from_days = django_filters.NumberFilter(method='from_days_method')
 
     def hashtag_comment_or_post(self, queryset, name, value):
         return (queryset.filter(hashtags__hashtag_title=value) |
                 queryset.filter(comment_post__hashtags__hashtag_title=value))
 
+    def from_days_method(self, queryset, name, value):
+        time_before = timezone.now() - datetime.timedelta(days=value)
+        return queryset.filter(created_at__lte=time_before)
+
     class Meta:
         model = Post
-        fields = ['address_id', 'hashtag_title']
+        fields = ['address_id', 'hashtag_title', 'from_days']
         search_fields = ['title', 'body']
